168bc44693d9c9a4112fadd66c0027411becba3a
sri2403 <sriharinimuthukrishnan@gmail.com>
2024-02-15 23:20:24 +0530

    second

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = ({ cartCount }) => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container px-4 px-lg-5">
-        <a className="navbar-brand" href="#!">Start Bootstrap</a>
+        <a className="navbar-brand" href="#!">Shopping</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
